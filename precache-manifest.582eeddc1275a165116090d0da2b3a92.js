ffe0ef06fc35fead57f722bf36a4e92a2695b681
Aiyazkhan Pathan <pathan.ayaz@outlook.com>
2021-02-06 00:21:16 +0530

    Updates

---
+++ precache-manifest.582eeddc1275a165116090d0da2b3a92.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d301fe7ec7c8eef56fbdaa83f48da06d",
+    "url": "/medicine-application/index.html"
+  },
+  {
+    "revision": "ce173927b6edc93dc23e",
+    "url": "/medicine-application/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "9fb8289eadb129392a6c",
+    "url": "/medicine-application/static/css/main.03333e94.chunk.css"
+  },
+  {
+    "revision": "ce173927b6edc93dc23e",
+    "url": "/medicine-application/static/js/2.425bb70b.chunk.js"
+  },
+  {
+    "revision": "9fb8289eadb129392a6c",
+    "url": "/medicine-application/static/js/main.39afe7c6.chunk.js"
+  },
+  {
+    "revision": "8ad0a0e17ccc6c66971e",
+    "url": "/medicine-application/static/js/runtime-main.840aacc6.js"
+  }
+]);
